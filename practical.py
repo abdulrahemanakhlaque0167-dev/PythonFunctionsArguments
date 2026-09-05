b61679bed4439c908bf63b98a46e83f73d4323c6
zohaibgame4444-sketch <zohaib.game4444@gmail.com>
2026-09-05 17:45:47 +0530

    nested loop practical created

---
+++ practical.py
@@ -0,0 +1,21 @@
+'''
+f=int(input("enter the number for increaing lines: "))
+
+for i in range(1,f+1):
+    for j in range(i):
+        print('*',end=' ')
+    print()
+
+g=int(input("enter the number for decreasing lines: "))
+for i in range(g,0,-1):
+    for j in range(i):
+        print('*',end=' ')
+    print()
+'''
+h=int(input("enter the number for loop to run: "))
+n=1
+for i in range(1,h+1):
+    for j in range(i):
+        print(n,end=' ')
+        n+=1
+    print()
