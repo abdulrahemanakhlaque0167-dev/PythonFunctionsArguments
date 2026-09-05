348850e8dfba8a211480ba9d4cefd9df71a92654
zohaibgame4444-sketch <zohaib.game4444@gmail.com>
2026-09-05 17:54:56 +0530

    removed the ''

--- practical.py
+++ practical.py
@@ -1,4 +1,4 @@
-'''
+
 f=int(input("enter the number for increaing lines: "))
 
 for i in range(1,f+1):
@@ -11,7 +11,7 @@ for i in range(g,0,-1):
     for j in range(i):
         print('*',end=' ')
     print()
-'''
+
 h=int(input("enter the number for loop to run: "))
 n=1
 for i in range(1,h+1):
