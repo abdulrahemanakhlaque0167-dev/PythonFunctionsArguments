a6365175b022268aa34f7ef76d926f5c1a64fd0b
zohaibgame4444-sketch <zohaib.game4444@gmail.com>
2026-09-06 18:05:54 +0530

    a number guessing game's code written

---
+++ number_guess.py
@@ -0,0 +1,22 @@
+import random
+f=random.randint(1,100)
+point=100
+print("welcome to the number guessing game")
+print("you have ten tries to guess the number")
+print("each wrong decreases 10 point from total 100 points")
+print("try to guess it as early as possible")
+for i in range(10):
+    g=int(input("enter the guess: "))
+    if g==f:
+        print("you won in",i,"attemps")
+        print("your point is=",point)
+        break
+    elif g>=f:
+        print("your guess is high")
+        point-=10
+    else:
+        print("your guess is low")
+        point-=10
+else:
+    print("you lost")
+    print("you got 0 points")
