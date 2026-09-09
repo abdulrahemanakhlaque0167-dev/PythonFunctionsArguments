2602a3983a4de7660abdb8a631a6775ef3e37fed
zohaibgame4444-sketch <zohaib.game4444@gmail.com>
2026-09-09 17:37:09 +0530

    age calculator code file created

---
+++ age_calculator.py
@@ -0,0 +1,6 @@
+a=int(input("enter the year you were born: "))
+h=2026-a
+if a<=1900:
+    print("not a possible year you were born")
+else:
+    print("if you were born in the year",a,"your age is",h)
